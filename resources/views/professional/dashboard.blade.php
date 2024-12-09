480faba63f17a43bc4b6b4a6457daea1d24a19dc
Marc Austin <121754521+MarcAusss@users.noreply.github.com>
2024-12-09 16:41:12 +0800

    new

--- resources/views/professional/dashboard.blade.php
+++ resources/views/professional/dashboard.blade.php
@@ -1,29 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Speak</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    <!-- Icons -->
-    <script src="https://kit.fontawesome.com/a1d595f7c6.js" crossorigin="anonymous"></script>
-
-    <!-- Tailwind CSS -->
-    @vite('resources/css/app.css')
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-</head>
-
-<body class="font-sans antialiased dark:bg-black dark:text-white/50">
-    <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
-        
-    @include('professional.layouts.navigation') <!-- Adjust to use the professional navigation -->
-
+<x-professional-layout>
+    <div>
+    @section('professional_content')
 
         <!---------------------- Landing Section ---------------------->
 
@@ -775,7 +752,6 @@
 
 
 
-    </div>
 
 
     <style>
@@ -952,7 +928,6 @@
 
         });
     </script>
-
-</body>
-
-</html>
+        @stop
+    </div>
+</x-professional-layout>
