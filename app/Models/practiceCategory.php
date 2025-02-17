e115178de4fff6370e96f9ea4e6ea94013da22a8
Arvin Milan <arvingm1522@gmail.com>
2025-02-17 20:23:48 +0800

    Added Models for User Practice

--- app/Models/practiceCategory.php
+++ app/Models/practiceCategory.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
-class practiceCategory extends Model
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+class PracticeCategory extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function exercises()
+    {
+        return $this->hasMany(PracticeExercise::class);
+    }
 }
--- app/Models/practiceExercise.php
+++ app/Models/practiceExercise.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
-class practiceExercise extends Model
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+class PracticeExercise extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'practiceCategory_id', 'media_file'];
+
+    public function category()
+    {
+        return $this->belongsTo(PracticeCategory::class);
+    }
 }
